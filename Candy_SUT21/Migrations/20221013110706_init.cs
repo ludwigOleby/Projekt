ecefdb87d46409d66fe12d73632c799504593c39
Ludwig Oleby <oleby.ludwig@gmail.com>
2022-10-13 13:09:28 +0200

    initial push containing database connection

---
+++ Candy_SUT21/Migrations/20221013110706_init.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Candy_SUT21.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
