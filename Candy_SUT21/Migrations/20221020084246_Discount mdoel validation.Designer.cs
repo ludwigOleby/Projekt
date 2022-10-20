da5c3f1ee0c3246f3e59abd9dc64261f6621836b
Robin Svensson <robinesvensson@gmail.com>
2022-10-20 11:44:28 +0200

    -Added validation to Discount

--- Candy_SUT21/Migrations/20221020084246_Discount mdoel validation.Designer.cs
+++ Candy_SUT21/Migrations/20221020084246_Discount mdoel validation.Designer.cs
@@ -4,14 +4,16 @@ using Candy_SUT21.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Candy_SUT21.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221020084246_Discount mdoel validation")]
+    partial class Discountmdoelvalidation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Candy_SUT21/Models/Discount.cs
+++ Candy_SUT21/Models/Discount.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Candy_SUT21.Models
 {
     public class Discount
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Discount name is required!")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "Name must be more than 2 characters and less than 25!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Percentage is required!")]
+        [Range(0, 100, ErrorMessage = "Percentage must be between 0 and 100!")]
         public int Percentage { get; set; }
+        [Required(ErrorMessage="Start date is required!")]
         public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "End date is required!")]
         public DateTime EndDate { get; set; }
         public IEnumerable<Candy> Candies { get; set; }
 
         public bool IsActive()
         {
-            return StartDate >= DateTime.Now && EndDate <= DateTime.Now; 
+            return StartDate <= DateTime.Now && EndDate >= DateTime.Now; 
         }
     }
 }
