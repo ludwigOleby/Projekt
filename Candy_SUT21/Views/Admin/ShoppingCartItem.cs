b33c0244dbdb274fc3dca87724a60026123511e5
Robin Svensson <robinesvensson@gmail.com>
2022-10-27 16:06:27 +0200

    -Remade navbar when presented to smaller screens -Added validation for new user -Made discount prices show with currency format

---
+++ Candy_SUT21/Views/Admin/ShoppingCartItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Candy_SUT21.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int ShoppingCartItemsId { get; set; }
+        public string ShoppingCartId { get; set; }
+        public Candy Candy { get; set; }
+        public int Amount { get; set; }
+
+    }
+}
